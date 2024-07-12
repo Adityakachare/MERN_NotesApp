bc59b0c1df88eb07d104d6919bdd52433859dcff
Aditya Kachare <aadityakachare132@gmail.com>
2024-07-12 23:20:47 +0530

    MyNotes page  and some routes

---
+++ frontend/src/components/MainScreen.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import "./MainScreen.css";
+
+const MainScreen = ({ title, children }) => {
+  return (
+    <div className="mainBack">
+      <Container>
+        <Row>
+          <div className="page">
+            {title && (
+              <>
+                <h1 className="heading">{title}</h1>
+                <hr />
+              </>
+            )}
+            {/* {user && (
+              <>
+                <h1 className="heading">{user}</h1>
+                <hr />
+              </>
+            )} */}
+
+            {children}
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default MainScreen;
