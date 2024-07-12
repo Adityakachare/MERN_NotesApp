f6eb980d87ca2847d13eed3d0c519d51dcc1fb50
Aditya Kachare <aadityakachare132@gmail.com>
2024-07-12 17:14:41 +0530

    Header Footer Landing Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
 
 const App = () => (
-  <div className="App">
-    <h1>Hello</h1>
-    <h1>Hello</h1>
-  </div>
+  <>
+    <Header />
+    <main>
+      <LandingPage />
+    </main>
+    <Footer />
+  </>
 );
 
 export default App;
